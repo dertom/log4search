c00f3c2a5b21e7af87967ecf5aca6c44fd157e71
tom <systom@outlook.de>
2021-12-13 20:23:33 +0100

    sudo added

--- get-log4j.sh
+++ get-log4j.sh
@@ -3,7 +3,8 @@
 SEARCH_PATTERN=log4j2*
 SERVERS_FILE="$(dirname '$0')"/servers.txt
 SSH_OPTIONS="-n -o StrictHostKeyChecking=no -p587"
-RUSER=root
+RUSER=foobar
+SUDO=yes
 
 if [[ ! -f $SERVERS_FILE ]]; then 
   echo "ERROR: $SERVERS_FILE not exists!"
@@ -13,7 +14,14 @@ fi
 LOGFILE=$(dirname "$0")/findings-log4j.txt
 echo > $LOGFILE
 
-while read SERVER; do
-  echo -n "$SERVER;" >> $LOGFILE
-  ssh $SSH_OPTIONS $RUSER@$SERVER "find / -type f -name $SEARCH_PATTERN" >> $LOGFILE
-done < $SERVERS_FILE
+if [[ $SUDO == "yes" ]]; then
+  while read SERVER; do
+    echo -n "$SERVER;" >> $LOGFILE
+    ssh $SSH_OPTIONS $RUSER@$SERVER "sudo su -; find / -type f -name $SEARCH_PATTERN" >> $LOGFILE
+  done < $SERVERS_FILE
+else
+  while read SERVER; do
+    echo -n "$SERVER;" >> $LOGFILE
+    ssh $SSH_OPTIONS $RUSER@$SERVER "find / -type f -name $SEARCH_PATTERN" >> $LOGFILE
+  done < $SERVERS_FILE
+fi
