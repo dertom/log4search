23ab809c9d9b9d611e6aa7e2ad03999a3e70eebb
tom <systom@outlook.de>
2021-12-13 20:15:08 +0100

    some changes ;)

--- get-log4j.sh
+++ get-log4j.sh
@@ -2,7 +2,8 @@
 
 SEARCH_PATTERN=log4j2*
 SERVERS_FILE="$(dirname '$0')"/servers.txt
-
+SSH_OPTIONS="-n -o StrictHostKeyChecking=no -p587"
+RUSER=root
 
 if [[ ! -f $SERVERS_FILE ]]; then 
   echo "ERROR: $SERVERS_FILE not exists!"
@@ -14,5 +15,5 @@ echo > $LOGFILE
 
 while read SERVER; do
   echo -n "$SERVER;" >> $LOGFILE
-  ssh -n -o StrictHostKeyChecking=no -p587 root@$SERVER "find / -type f -name $SEARCH_PATTERN" >> $LOGFILE
+  ssh $SSH_OPTIONS $RUSER@$SERVER "find / -type f -name $SEARCH_PATTERN" >> $LOGFILE
 done < $SERVERS_FILE
